2cdab36fe73cd0b96b8538028ac0dcbbe0d66d27
Spigushe <martin.cuchet@gmail.com>
2023-08-06 10:53:00 +0200

    Update conf.py

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -6,27 +6,23 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'MTGDC-BANLIST'
-copyright = '2023, Spigushe'
-author = 'Spigushe'
-release = '1.0.0'
+project = "MTGDC-BANLIST"
+copyright = "2023, Spigushe"
+author = "Spigushe"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx_rtd_theme',
-    'sphinx.ext.githubpages',
-    'sphinx.ext.autodoc'
-]
+extensions = ["sphinx_rtd_theme", "sphinx.ext.githubpages", "sphinx.ext.autodoc"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-language = 'fr'
+language = "fr"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
